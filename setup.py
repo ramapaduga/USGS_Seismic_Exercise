e179128d9aedb88a032e7b8edc0e79c0000e434d
ramapaduga <71598392+ramapaduga@users.noreply.github.com>
2020-09-21 08:32:16 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     description="Git pull from master branch",
     version="0.0.1",
     packages=find_packages(),
-    package_data={['dag_api_jobs/cost_reportjob/*.json','dag_api_jobs/cost_reportjob/*.dat','dag_api_jobs/GoogleDoubleClick/*.json','dag_api_jobs/GoogleDoubleClick/*.dat','dag_api_jobs/GoogleDoubleClick/*.txt']},
     author="Ramanathan Ramadurai",
     author_email="ramapaduga@gmail.com",
     url="https://github.com/ramapaduga/USGS_Seismic_Exercise"
